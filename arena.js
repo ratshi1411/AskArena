605d4c0e0a5f7fd257c9cf3981ccedccdaca44a5
ratshi1411 <you@example.com>
2025-06-09 21:05:31 +0530

    Initial Commit

--- arena.js
+++
@@ -1,5 +0,0 @@
-const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-recognition.onresult = (event) => {
-  document.getElementById("userInput").value = event.results[0][0].transcript;
-  handleUserInput();
-};
